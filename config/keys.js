c9143c23d9bf0450f784abdc901174e3ef55b826
VinothKumar <89235649+vinowoualabs@users.noreply.github.com>
2022-06-20 11:49:25 +0530

    Project Completed

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
+  mongoURI:
+    "mongodb+srv://achu:atchu1112@cluster0.qm46ese.mongodb.net/?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
